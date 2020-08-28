0528c9b4e50f6d5f23405f67e54df0c4dfd9ddf5
HiepCongChua <hieplevuc@gmail.com>
2020-08-28 09:55:35 +0700

    update project

--- base_react/src/index.js
+++ base_react/src/index.js
@@ -1,11 +1,42 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./containers/App.js"
-import registerServiceWorker from "./registerServiceWorker"
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import "./index.css"
+// import App from "./containers/App.js"
+// import registerServiceWorker from "./registerServiceWorker"
 
-ReactDOM.render(
-  <App initProps="Hello App.js" />,
-  document.getElementById("root")
-)
-registerServiceWorker()
+import React, { useEffect } from "react"
+import { render } from "react-dom"
+//===================================
+const myContainerStyle = {
+  maxWidth: "200px",
+  margin: "5px auto 0",
+  padding: "5px 0",
+  backgroundColor: "#ddd",
+  textAlign: "center",
+  border: "1px solid #000",
+  fontSize: "16px",
+  fontFamily: "Helvetica",
+}
+//===================================
+const MyUseEffect = ({ myTestProps }) => {
+  useEffect(() => {
+    console.log("useEffect")
+  }, [myTestProps])
+  return <div>{myTestProps}</div>
+}
+//===================================
+const MyApp = ({ myProps }) => {
+  return (
+    <div style={myContainerStyle}>
+      See the console
+      <MyUseEffect myTestProps="My test" />
+    </div>
+  )
+}
+render(<MyApp />, document.getElementById("root"))
+
+// ReactDOM.render(
+//   <App initProps="Hello App.js" />,
+//   document.getElementById("root")
+// )
+// registerServiceWorker()
